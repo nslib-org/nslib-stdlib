7f9c647d98f6d51f9491bdb46aba6f5ab1c62acc
Kota Mizushima <kmizu.main@gmail.com>
2026-03-12 04:33:15 +0900

    fix: Scala 2.13.16 for Scala.js 1.16.0 compat, Scala Native 0.5.10 for Scala 3.3.7 support

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,10 +1,10 @@
 object Versions {
-  val scala213     = "2.13.18"
+  val scala213     = "2.13.16"
   val scala3Lts    = "3.3.7"
   val scala3Latest = "3.5.2" // update to latest non-LTS Scala 3 as needed
 
   val scalaJs     = "1.16.0"
-  val scalaNative = "0.5.5"
+  val scalaNative = "0.5.10"
 
   val munit = "1.0.3"
 }
