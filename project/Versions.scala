f7b24704b3b8eea7954ae56aa0f38cf695477918
Kota Mizushima <kmizu.main@gmail.com>
2026-03-12 04:34:30 +0900

    fix: upgrade Scala.js to 1.20.2 and Scala Native to 0.5.10 for version compatibility

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object Versions {
   val scala3Lts    = "3.3.7"
   val scala3Latest = "3.5.2" // update to latest non-LTS Scala 3 as needed
 
-  val scalaJs     = "1.16.0"
+  val scalaJs     = "1.20.2"
   val scalaNative = "0.5.10"
 
   val munit = "1.0.3"
